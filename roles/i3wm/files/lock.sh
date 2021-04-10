6edee7465174170bfdaecac500fe727faa73d69a
Thibault Hazelart <thibault.hazelart@continental-corporation.com>
2021-04-10 20:05:52 +0200

    feat: add ubuntu handlers

--- roles/i3wm/files/lock.sh
+++ roles/i3wm/files/lock.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -e
+IMG_FILE=/tmp/screen_locked.png
 
+rm -f ${IMG_FILE}
 # screenshot!
-scrot /tmp/screen_locked.png
+scrot ${IMG_FILE}
 
 # Blur it
-#convert /tmp/screen_locked.png -blur 0x6 /tmp/screen_locked.png
-convert /tmp/screen_locked.png -scale 10% -scale 1000% /tmp/screen_locked.png
+#convert ${IMG_FILE} -blur 0x6 ${IMG_FILE}
+convert ${IMG_FILE} -scale 10% -scale 1000% ${IMG_FILE}
 
 # Lock screen with blurred image
-i3lock -e -n -i /tmp/screen_locked.png
+i3lock -e -n -i ${IMG_FILE}
